ae6996800bf63d7762ef224f375527f25584554b
kubrakovD <kubrakov.d@gmail.com>
2016-05-11 11:57:02 +0300

    2 commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 ;(function($){
 
+	$(document).ready(function(){
+
+	var $ipadSlider = $('.ba-ipad-slider');
+		$ipadSlider.slick({
+			dots:false,
+			infinite:false
+		});
+	})
+
+
+	var $bigSlider = $('.ba-big-slider');
+	var $textSlider = $('.ba-text-slider');
+	$('.ba-text-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		asNavFor: '.ba-big-slider'
+	});
+	$('.ba-big-slider').slick({
+		  slidesToShow: 4,
+		  slidesToScroll: 1,
+		  asNavFor: '.ba-text-slider',
+		  arrows:false,
+		  dots: false,
+		  centerMode: true,
+		  focusOnSelect: true
+	});
+
+
+
+
+
+
+
 	$(window).load(function(){
 	    var mapDiv = $('#map') [0];
 		var coordinates = {lat: 45.4783789, lng: 9.1230817};
@@ -21,6 +56,9 @@
 		infowindow.open(map, marker);
 	})
 
-
-	$('.swipebox').swipebox();
+	$( '.swipebox' ).swipebox();
 })(jQuery);
+
+
+
+
